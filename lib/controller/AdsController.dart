82b423c7905439ab56dac4dcf7f763f4ea5c204a
Ashish Verma <ashish1500616@gmail.com>
2021-06-27 13:17:08 +0530

    Add Banner Ad 4 and Banner Ad 5

--- lib/controller/AdsController.dart
+++ lib/controller/AdsController.dart
@@ -6,9 +6,13 @@ class AdsController extends GetxController {
   late BannerAd bannerAdFirst;
   late BannerAd bannerAdSecond;
   late BannerAd bannerAdThird;
+  late BannerAd bannerAdFourth;
+  late BannerAd bannerAdFifth;
   static const BANNER_UNIT_1 = "ca-app-pub-5225835586845251/9410986702";
   static const BANNER_UNIT_2 = "ca-app-pub-5225835586845251/9259777616";
   static const BANNER_UNIT_3 = "ca-app-pub-5225835586845251/2552814756";
+  static const BANNER_UNIT_4 = "ca-app-pub-5225835586845251/8199360278";
+  static const BANNER_UNIT_5 = "ca-app-pub-5225835586845251/3049307078";
   static const INTERSTITIAL_UNIT_1 = "ca-app-pub-5225835586845251/8926651414";
 
   @override
@@ -29,17 +33,13 @@ class AdsController extends GetxController {
           onAdLoaded: (Ad ad) => () {
             print('Ad loaded.');
           },
-          // Called when an ad request failed.
           onAdFailedToLoad: (Ad ad, LoadAdError error) {
             // Dispose the ad here to free resources.
             ad.dispose();
             print('Ad failed to load: $error');
           },
-          // Called when an ad opens an overlay that covers the screen.
           onAdOpened: (Ad ad) => print('Ad opened.'),
-          // Called when an ad removes an overlay that covers the screen.
           onAdClosed: (Ad ad) => print('Ad closed.'),
-          // Called when an impression occurs on the ad.
           onAdImpression: (Ad ad) => print('Ad impression.'),
         ));
     bannerAdFirst.load();
@@ -51,21 +51,15 @@ class AdsController extends GetxController {
         request: AdRequest(),
         size: AdSize.largeBanner,
         listener: BannerAdListener(
-          // Called when an ad is successfully received.
           onAdLoaded: (Ad ad) => () {
             print('Ad loaded.');
           },
-          // Called when an ad request failed.
           onAdFailedToLoad: (Ad ad, LoadAdError error) {
-            // Dispose the ad here to free resources.
             ad.dispose();
             print('Ad failed to load: $error');
           },
-          // Called when an ad opens an overlay that covers the screen.
           onAdOpened: (Ad ad) => print('Ad opened.'),
-          // Called when an ad removes an overlay that covers the screen.
           onAdClosed: (Ad ad) => Get.toNamed("/watchVideo"),
-          // Called when an impression occurs on the ad.
           onAdImpression: (Ad ad) => print('Ad impression.'),
         ));
 
@@ -78,25 +72,61 @@ class AdsController extends GetxController {
         request: AdRequest(),
         size: AdSize.largeBanner,
         listener: BannerAdListener(
-          // Called when an ad is successfully received.
           onAdLoaded: (Ad ad) => () {
             print("Banner Third Is Ready");
             print('Ad loaded.');
           },
-          // Called when an ad request failed.
           onAdFailedToLoad: (Ad ad, LoadAdError error) {
-            // Dispose the ad here to free resources.
             ad.dispose();
             print('Ad failed to load: $error');
           },
-          // Called when an ad opens an overlay that covers the screen.
           onAdOpened: (Ad ad) => print('Ad opened.'),
-          // Called when an ad removes an overlay that covers the screen.
           onAdClosed: (Ad ad) => Get.toNamed("/watchVideo"),
-          // Called when an impression occurs on the ad.
           onAdImpression: (Ad ad) => print('Ad impression.'),
         ));
     bannerAdThird.load();
+  }
+
+  createAndLoadFourthBannerAd() {
+    bannerAdFourth = BannerAd(
+        adUnitId: BANNER_UNIT_4,
+        request: AdRequest(),
+        size: AdSize.largeBanner,
+        listener: BannerAdListener(
+          onAdLoaded: (Ad ad) => () {
+            print("Banner Third Is Ready");
+            print('Ad loaded.');
+          },
+          onAdFailedToLoad: (Ad ad, LoadAdError error) {
+            ad.dispose();
+            print('Ad failed to load: $error');
+          },
+          onAdOpened: (Ad ad) => print('Ad opened.'),
+          onAdClosed: (Ad ad) => Get.toNamed("/watchVideo"),
+          onAdImpression: (Ad ad) => print('Ad impression.'),
+        ));
+    bannerAdFourth.load();
+  }
+
+  createAndLoadFifthBannerAd() {
+    bannerAdFifth = BannerAd(
+        adUnitId: BANNER_UNIT_5,
+        request: AdRequest(),
+        size: AdSize.largeBanner,
+        listener: BannerAdListener(
+          onAdLoaded: (Ad ad) => () {
+            print("Banner Third Is Ready");
+            print('Ad loaded.');
+          },
+          onAdFailedToLoad: (Ad ad, LoadAdError error) {
+            ad.dispose();
+            print('Ad failed to load: $error');
+          },
+          onAdOpened: (Ad ad) => print('Ad opened.'),
+          onAdClosed: (Ad ad) => Get.toNamed("/watchVideo"),
+          onAdImpression: (Ad ad) => print('Ad impression.'),
+        ));
+    bannerAdFifth.load();
   }
 
   @override
@@ -106,14 +136,12 @@ class AdsController extends GetxController {
 
   createInterstitialAd() {
     InterstitialAd.load(
-      adUnitId: 'ca-app-pub-3940256099942544/1033173712',
+      adUnitId: INTERSTITIAL_UNIT_1,
       request: AdRequest(),
       adLoadCallback: InterstitialAdLoadCallback(
-        // if ad fails to load
         onAdFailedToLoad: (LoadAdError error) {
           print('Ad exited with error: $error');
         },
-        // else
         onAdLoaded: (InterstitialAd ad) {
           this.myInterstitial = ad; // set the ad equal to the current ad
         },
@@ -122,27 +150,11 @@ class AdsController extends GetxController {
   }
 
   showInterstitialAd() {
-    // create callbacks for ad
     myInterstitial!.fullScreenContentCallback = FullScreenContentCallback(
-      // when dismissed
       onAdDismissedFullScreenContent: (InterstitialAd ad) {
-        /*Navigator.push(
-          Get.context!,
-          MaterialPageRoute(
-            builder: (context) => WatchVideo(), // go to next page
-          ),
-        );*/
         ad.dispose(); // dispose of ad
       },
-
-      // if ad fails to show content
       onAdFailedToShowFullScreenContent: (InterstitialAd ad, AdError error) {
-        /*Navigator.push(
-          Get.context!,
-          MaterialPageRoute(
-            builder: (context) => WatchVideo(), // go to next page
-          ),
-        );*/
         print('$ad onAdFailedToShowFullScreenContent: $error'); // print error
         ad.dispose(); // dispose ad
       },
